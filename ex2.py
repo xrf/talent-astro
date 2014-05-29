85822ad4ac92a5d346c009e4b7e3b8ab6e53e5c6
Fei Yuan <yuan@nscl.msu.edu>
2014-05-29 18:04:45 -0400

    Add ability to edit gravitational potential

--- ex2.py
+++ ex2.py
@@ -13,8 +13,9 @@ def write_file(filename, s):
 
 FNULL = open(os.devnull, 'w')
 
+use_gravity = False
 prefix = "Sedov"
-num_cycles = 4500
+num_cycles = 100#4500
 t_end = 9.9e19
 t_print = t_end
 t_movie = t_end
@@ -33,6 +34,18 @@ blast_radius = dx
 blast_pressure = blast_energy / (4. * math.pi / 3. * blast_radius ** 3) * (gamma - 1.)
 assert abs(blast_pressure - 1.59e6) < 0.01e6
 
+if use_gravity:
+    potential = "-1. / xf(n) ** 2"
+else:
+    potential = "0."
+
+s = read_file("VH1/src/PPMLR/forces.f90")
+# note: need to find a way to interpret '\10' as '\1' + '0'
+s = re.sub(r"^([\t ]*grav *\( *n *\) *=)[^\n]*$",
+           r"\1 " + potential.replace("\\", "\\\\"),
+           s, flags=re.MULTILINE)
+write_file("VH1/src/PPMLR/forces.f90", s)
+
 os.chdir("VH1/src/Starter")
 write_file("vhone.f90", read_file("../../../ex2-vhone.f90").format(**locals()))
 write_file("../../indat", '''
@@ -47,7 +60,7 @@ write_file("../../indat", '''
  tprin   = {t_print}
  tmovie  = {t_movie} /
 '''.strip().format(**locals()) + "\n")
-subprocess.check_call(["make"])
+subprocess.check_call(["make"], stdout=FNULL)
 os.chdir("../../..")
 
 os.chdir("VH1")
@@ -59,6 +72,16 @@ os.mkdir("output")
 subprocess.check_call(["./vh1-starter"])
 os.chdir("..")
 
+r = re.compile("^Wrote +{prefix}([0-9]+) +to disk at time ="
+               .format(prefix=re.escape(prefix)) +
+               " *([-+.0-9eE]+) *\(ncycle = *[0-9]+ *\)$")
+time_map = {1000: 0}
+with open("VH1/output/{prefix}.hst".format(prefix=prefix)) as f:
+    for line in f:
+        m = r.match(line.strip())
+        if m:
+            index, time = m.groups()
+            time_map[int(index)] = float(time)
 try:
     for i in range(1000, 9999):
         data = df.DataFrame()
@@ -68,6 +91,7 @@ try:
         data.remap_names({str(k): v for k, v in enumerate(cols)})
         data.plot(
             filename="VH1/output/{prefix}{index}.png".format(prefix=prefix, index=i),
+            title="time = {}".format(time_map[i]),
             #ys=[1],
             y_range=(0, 5),
         )
